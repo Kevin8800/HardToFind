55ebbe45d0a49858c2b743a184858dbbb55b8dc2
Kevin <swang84@yorku.ca>
2012-12-04 16:16:48 -0500

    Bug fixing

--- eFRedBlue/src/util/Constants.java
+++ eFRedBlue/src/util/Constants.java
@@ -12,13 +12,19 @@ import java.text.DecimalFormat;
 		
 public  class Constants 
 {
+	public static final int number_of_retry = 9;
+	
 	public static final String XMLEXTENSION = ".xml";
 	public static final String FOLDERTOEXPORT = "export/";
-	
-	
 	public static final String MYERRORPAGE = "/myError.jspx";
-	
 	public static final DecimalFormat twoDForm = new DecimalFormat("#.##");
-
 	public static final DecimalFormat oneDForm = new DecimalFormat("#.#");
+	
+	
+	public static final String SHOPPINGCART_PATH = "/ShoppingCart";
+	public static final String LOGIN_PATH = "/Login";
+
+
 }
+
+
